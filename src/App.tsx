99764f44a09a60d7169bb1aee3596ed0c8fa6413
niksedov24 <nik.sedov890@gmail.com>
2025-09-16 19:45:46 +0400

    1 commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <h1 className="font-montserrat">hello world</h1>
+      <h1 className="font-dmsans">hello world</h1>
+    </>
+  );
+}
+
+export default App;
